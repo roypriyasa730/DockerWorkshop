a2d9366287fab461080459cfceed7c67e3a869e9
roypriyasa <roypriyasa730@gmail.com>
2025-06-24 16:38:57 +0530

    Add Dockerfile and ApiPingController for ServiceOne with updated endpoint

--- ServiceOne/src/main/java/com/example/ServiceOne/ApiPingController.java
+++ ServiceOne/src/main/java/com/example/ServiceOne/ApiPingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/ping")
+@RequestMapping("/serviceone/ping")
 public class ApiPingController {
 
     @GetMapping

5993a5b729985555439b7914dbd0fa14e0b9afe7
roypriyasa <roypriyasa730@gmail.com>
2025-06-27 17:13:15 +0530

    Refactor ApiPingController: unify endpoint path to '/ping' for ServiceOne and ServiceTwo

--- ServiceTwo/src/main/java/com/example/ServiceTwo/ApiPingController.java
+++ ServiceTwo/src/main/java/com/example/ServiceTwo/ApiPingController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/servicetwo/ping")
+@RequestMapping("/ping")
 public class ApiPingController {
     @GetMapping
     public String get(){
